4b25b564a44a259dcefaaba64892fdd5fcdff727
Stephen Leander <stephen.leander1@gmail.com>
2022-05-09 15:11:20 +0200

    move list display functions to index.js & create add todo function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
-import {List, makeNewListButton} from './list.js';
-
+import {List} from './list.js';
+import {ToDo} from './todo.js'
 
 // Header
 const header = document.createElement('div');
@@ -10,31 +10,92 @@ title.innerText = '✔ To-Do List';
 header.appendChild(title)
 document.body.appendChild(header);
 
-
-
-
 // Sidebar
 const sidebar = document.createElement('div');
 sidebar.classList.toggle('sidebar')
 document.body.appendChild(sidebar);
 
-// Create default list 'Tasks'
-const tasks = new List('Tasks');
-tasks.addToSidebar();
-
-// Add new list to sidebar
-
-
-
-
-
-makeNewListButton()
-
-
-
 // Content
 const content = document.createElement('div');
 content.classList.toggle('content')
 document.body.appendChild(content);
+
+
+
+// Create empty array to store lists
+let listArray = [];
+
+
+// Creates button that generates new list input on click
+function makeNewListButton() {
+    const newListButton = document.createElement('button');
+    newListButton.type = 'button';
+    newListButton.textContent = 'New List +'
+    newListButton.addEventListener('click', () => {
+        sidebar.removeChild(newListButton);
+        newListInput();
+    });
+    sidebar.appendChild(newListButton);
+}
+
+// Creates input field to name new list and buttons to add or cancel
+function newListInput() {
+    const inputContainer = document.createElement('div');
+
+    const listName = document.createElement('input');
+    inputContainer.appendChild(listName);
+
+    const addButton = document.createElement('button');
+    addButton.type = 'button';
+    addButton.classList.toggle('add');
+    addButton.textContent = 'Add';
+    addButton.addEventListener('click', () => {
+        if (listName.value == '') {
+            alert('Enter a name for the list');
+        } else { 
+            sidebar.removeChild(inputContainer);
+            const currentActiveButton = document.querySelector('.active');
+            currentActiveButton.classList.toggle('active');
+            const newList = new List(listName.value);
+            listArray.push(newList);
+            newList.addToSidebar();
+            makeNewListButton();
+        }
+    });
+    inputContainer.appendChild(addButton);
+
+    const cancelButton = document.createElement('button');
+    cancelButton.type = 'button'
+    cancelButton.classList.toggle('cancel');
+    cancelButton.textContent = 'Cancel'
+    cancelButton.addEventListener('click', () => {
+        sidebar.removeChild(inputContainer);
+        makeNewListButton();
+    });
+    inputContainer.appendChild(cancelButton);
+
+    sidebar.appendChild(inputContainer);
+}
+
+
+
+
+// Create default list 'Tasks'
+const tasks = new List('Tasks');
+listArray.push(tasks);
+tasks.addToSidebar();
+
+const test = new ToDo('test', '12/7', 'normal');
+const laundry = new ToDo('do laundry', 'today', 'high');
+tasks.addToDo(laundry);
+tasks.addToDo(test);
+tasks.display();
+// Add new list button to sidebar
+makeNewListButton()
+
+
+
+
+
 
 
--- src/list.js
+++ src/list.js
@@ -1,5 +1,5 @@
 export class List {
-    constructor (name) {
+    constructor(name) {
         this.name = name;
         this.tasks = [];
     }
@@ -17,61 +17,27 @@ export class List {
                 const currentActiveButton = document.querySelector('.active');
                 currentActiveButton.classList.toggle('active');
                 button.classList.toggle('active');
+                this.display();
             }
         });
         sidebar.appendChild(button);
     }
+
+    addToDo(toDo) {
+        this.tasks.push(toDo);
+    }
+
+    display() {
+        const content = document.querySelector('.content');
+        content.innerHTML = '';
+        const taskContainer = document.createElement('div');
+        this.tasks.forEach(task => {
+            const taskString = document.createElement('div');
+            taskString.textContent = `${task.name} | ${task.date}`;
+            taskContainer.appendChild(taskString);
+        });
+        content.appendChild(taskContainer);
+    }
+
 }
 
-// Creates button that generates new list input on click
-export function makeNewListButton() {
-    const sidebar = document.querySelector('.sidebar');
-    const newListButton = document.createElement('button');
-    newListButton.type = 'button';
-    newListButton.textContent = 'New List +'
-    newListButton.addEventListener('click', () => {
-        sidebar.removeChild(newListButton);
-        newListInput();
-    });
-    sidebar.appendChild(newListButton);
-}
-
-
-// Creates input field to name new list and buttons to add or cancel
-function newListInput() {
-    const sidebar = document.querySelector('.sidebar');
-    const inputContainer = document.createElement('div');
-
-    const listName = document.createElement('input');
-    inputContainer.appendChild(listName);
-
-    const addButton = document.createElement('button');
-    addButton.type = 'button';
-    addButton.classList.toggle('add');
-    addButton.textContent = 'Add';
-    addButton.addEventListener('click', () => {
-        if (listName.value == '') {
-            alert('Enter a name for the list');
-        } else { 
-            sidebar.removeChild(inputContainer);
-            const currentActiveButton = document.querySelector('.active');
-            currentActiveButton.classList.toggle('active');
-            const newList = new List(listName.value);
-            newList.addToSidebar();
-            makeNewListButton();
-        }
-    });
-    inputContainer.appendChild(addButton);
-
-    const cancelButton = document.createElement('button');
-    cancelButton.type = 'button'
-    cancelButton.classList.toggle('cancel');
-    cancelButton.textContent = 'Cancel'
-    cancelButton.addEventListener('click', () => {
-        sidebar.removeChild(inputContainer);
-        makeNewListButton();
-    });
-    inputContainer.appendChild(cancelButton);
-
-    sidebar.appendChild(inputContainer);
-}
